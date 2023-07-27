1fcaec5ba77b94cb22fe8886ef97c925b06a68da
Nailton Almeida <42894763+nailton-almeida@users.noreply.github.com>
2023-07-27 16:41:49 -0300

    Add files via upload

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,53 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+      
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                
+                throw new ArgumentException("A quantidade de hóspedes superar a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valorTotal = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >=10)
+            {
+                valorTotal = (decimal)(DiasReservados - (DiasReservados * 0.5));
+            }
+
+            return valorTotal;
+        }
+    }
+}
