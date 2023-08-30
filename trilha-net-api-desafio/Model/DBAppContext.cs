470ff534c2ebea1093304770b146afc01f8017c2
NailtonAlmeida <nailton.junior@mc1.com.br>
2023-08-30 12:07:59 -0300

    feat: organizacao diretorio

---
+++ trilha-net-api-desafio/Model/DBAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace trilha_net_api_desafio.Model;
+
+public class DBAppContext : DbContext
+{
+    public DBAppContext(DbContextOptions<DBAppContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Tarefa>? Tarefas { get; set; }
+
+}
---
+++ trilha-net-api-desafio/Repositories/ITarefa.cs
@@ -0,0 +1,17 @@
+﻿using trilha_net_api_desafio.Model;
+
+namespace trilha_net_api_desafio.Repositories;
+
+public interface ITarefa
+{
+    Task<IEnumerable<Tarefa>> Get();
+    Task<Tarefa> Get(int id);
+
+    Task<List<Tarefa>> Get(string titulo);
+    Task<List<Tarefa>> Get(DateTime data);
+    Task<List<Tarefa>> Get(bool status);
+    Task<Tarefa> Create(Tarefa tarefa);
+    Task Delete(int id);
+    Task Update(Tarefa tarefa);
+
+}
